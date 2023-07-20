14f43ed6d2a635726c315ee1a773a4d31ef0013f
Krasimir Iliev <kvi_2012@abv.bg>
2023-07-20 21:54:56 +0300

    Fix log in

--- Vehicle-Manager-Platform/Vmp.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Vehicle-Manager-Platform/Vmp.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,14 +1,8 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
+﻿#nullable disable
 
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 using Vmp.Data.Models;
 
@@ -35,8 +29,6 @@ namespace Vmp.Web.Areas.Identity.Pages.Account
             }
             else
             {
-                // This needs to be a redirect so that the browser performs a new
-                // request and the identity for the user gets updated.
                 return RedirectToPage();
             }
         }
